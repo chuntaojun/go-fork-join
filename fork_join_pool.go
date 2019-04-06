d0c53b3f4e465ea95c52d090eb7f82301e2aebfa
廖春涛 <liaochuntao@liaochuntaos-MacBook-Pro.local>
2019-04-06 19:40:51 +0800

    feat: 添加异常退出 fork-join 任务

--- fork_join_pool.go
+++ fork_join_pool.go
@@ -1,45 +1,69 @@
 package fork_join
 
-import "sync"
+import (
+	"context"
+	"fmt"
+	"sync"
+)
 
 type ForkJoinPool struct {
-	Name		string
-	cap			int32
-	taskQueue	*TaskQueue
-	wp			*Pool
-	lock		sync.Mutex
-	signal		*sync.Cond
-	once		sync.Once
+	Name      		string
+	cap       		int32
+	taskQueue 		*TaskQueue
+	wp        		*Pool
+	lock      		sync.Mutex
+	signal    		*sync.Cond
+	once      		sync.Once
+	goroutineID		int32
+	ctx         	context.Context
+	cancel			context.CancelFunc
+	err				interface{}
 }
 
 func NewForkJoinPool(name string, workerCap int32) *ForkJoinPool {
+	ctx, cancel := context.WithCancel(context.Background())
 	fp := &ForkJoinPool{
 		Name:      name,
 		cap:       workerCap,
 		taskQueue: NewTaskQueue(workerCap),
+		ctx:	   ctx,
+		cancel:	   cancel,
 	}
-	fp.wp = NewPool()
+	fp.wp = newPool(cancel)
 	fp.signal = sync.NewCond(new(sync.Mutex))
-	fp.run()
+	fp.run(ctx)
 	return fp
 }
 
-func (fp *ForkJoinPool) pushTask(t Task, f *ForkJoinTask) {
-	fp.taskQueue.Enqueue(t, f)
+func (fp *ForkJoinPool) SetPanicHandler(panicHandler func(interface{}))  {
+	fp.wp.panicHandler = panicHandler
 }
 
-func (fp *ForkJoinPool) run() {
+func (fp *ForkJoinPool) pushTask(t Task, f *ForkJoinTask) {
+	fp.taskQueue.enqueue(t, f)
+}
+
+// 每个 worker 轮询自己对应的 Task 队列进行获取任务
+func (fp *ForkJoinPool) run(ctx context.Context) {
 	go func() {
 		wId := int32(0)
 		for {
-			if fp.taskQueue.HashNext(wId) {
-				job, ft := fp.taskQueue.DequeueByTali(wId)
-				fp.wp.retrieveWorker().job <- &struct {
-					T Task
-					F *ForkJoinTask
-				}{T: job, F: ft}
+			select {
+			case <-ctx.Done():
+				fmt.Printf("here is err")
+				fp.err = fp.wp.err
+				return
+			default:
+				hasTask, job, ft := fp.taskQueue.dequeueByTali(wId)
+				if hasTask {
+					fp.wp.retrieveWorker(ctx).job <- &struct {
+						T Task
+						F *ForkJoinTask
+						C context.Context
+					}{T: job, F: ft, C:ctx}
+				}
+				wId = (wId + 1) % fp.cap
 			}
-			wId = (wId + 1) % fp.cap
 		}
 	}()
 }
--- worker.go
+++ worker.go
@@ -1,29 +1,53 @@
 package fork_join
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 type Worker struct {
-	pool	*Pool
-	isRunning	bool
+	pool        *Pool
+	isRunning   bool
 	recycleTime time.Time
-	job		chan *struct{
-		T	Task
-		F	*ForkJoinTask
+	job         chan *struct {
+		T Task
+		F *ForkJoinTask
+		C context.Context
 	}
 }
 
-func (w *Worker) run() {
+func (w *Worker) run(ctx context.Context) {
 	go func() {
+
+		var tmpTask *ForkJoinTask
+
 		defer func() {
 			if p := recover(); p != nil {
+				w.pool.panicHandler(p)
+				if tmpTask != nil {
+					w.pool.err = p
+					close(tmpTask.result)
+				}
 			}
 		}()
-		for job := range w.job {
-			if job == nil {
-				w.pool.workerCache.Put(w)
+
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("An exception occurred and the task has stopped")
 				return
+			default:
+				for job := range w.job {
+					if job == nil {
+						w.pool.workerCache.Put(w)
+						return
+					}
+					tmpTask = job.F
+					job.F.result <- job.T.Compute()
+					panic("异常出现")
+				}
 			}
-			job.F.result <- job.T.Compute()
 		}
 	}()
 }
